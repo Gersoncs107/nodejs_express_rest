907c111f7d583eefcc8f469604f441188075a811
Gersoncs107 <gerson.silva107@gmail.com>
2025-08-19 22:52:30 -0300

    nodejs-express-update

--- models/note.js
+++ models/note.js
@@ -18,3 +18,11 @@ const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
