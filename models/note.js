aaceac3a8c37a36375603d41be51e316bd449631
Gersoncs107 <gerson.silva107@gmail.com>
2025-08-19 22:52:07 -0300

    nodejs-express-update

--- models/note.js
+++ models/note.js
@@ -5,3 +5,16 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_PASSWORD
 
 console.log('connecting to', url)
+
+mongoose.connect(url)
+.then(result => {
+    console.log('connected to MongoDB')
+})
+.catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
