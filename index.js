47e20df452bc0ab205594b0fb04df2203c373065
Gersoncs107 <gerson.silva107@gmail.com>
2025-08-23 22:46:32 -0300

    nodejs-express-update

--- index.js
+++ index.js
@@ -84,6 +84,21 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/notes/:id', (request, response, next) => {
  Note.findById(request.params.id)
  .then(result => {
