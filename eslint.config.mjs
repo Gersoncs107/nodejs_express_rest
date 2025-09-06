ac0ccc972d480ccc3f491360b60c2fe3ea4d3494
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-06 20:02:08 -0300

    nodejs-express-update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.js"],
+    ignores: ["node_modules", "dist", "build"],
     languageOptions: {
       globals: globals.node,
       ecmaVersion: 2022,
