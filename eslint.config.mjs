803284570c901118efa51ce77960346cb678cbe2
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-06 19:56:58 -0300

    nodejs-express-update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,15 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      // Add your custom rules here if needed
+    },
+  },
 ]);
