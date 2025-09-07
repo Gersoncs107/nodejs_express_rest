8ba5c88ae513225fa31460d50ef7febcaafed1bf
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-06 22:34:59 -0300

    nodejs-express-update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,11 +12,11 @@ export default defineConfig([
     },
     rules: {
       'eqeqeq': 'error',
-       'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
         'error', 'always'
     ],
-    'arrow-spacing': [
+      'arrow-spacing': [
         'error', { 'before': true, 'after': true }
     ]
     },
