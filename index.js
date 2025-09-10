2bb1c4c225cf5642fcf0f65fa9224a9c470794da
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-10 00:12:28 -0300

    nodejs-express-update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const Note = require('./models/note')
 
 app.use(express.json())
