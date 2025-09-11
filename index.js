c4be47973c4cfd72c7d4a0cfd2c9090cfec9563e
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-10 22:56:55 -0300

    nodejs-express-update

--- index.js
+++ index.js
@@ -93,7 +93,6 @@ const errorHandler = (error, request, response, next) => {
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT =  process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
 })
