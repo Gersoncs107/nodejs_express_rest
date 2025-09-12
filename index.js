4c596ca5f8513526b06f486caf3cdd637b93f466
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-11 23:00:05 -0300

    nodejs-express-update

--- index.js
+++ index.js
@@ -74,25 +74,6 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-const unknownEndpoint = ((request, response) => {
-    response.status(404).send({ error: 'Unknown Endpoint' })
-})
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
