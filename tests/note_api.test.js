1c888ab09bea917150e31c19cc4cbbc0982761f0
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-27 22:33:15 -0300

    nodejs-express-update

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+    await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+afterAll(() => {
+    mongoose.connection.close()
+})
