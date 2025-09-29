26e347d0d7eb96c081f8d704ec918a8aaebd6b4b
Gersoncs107 <gerson.silva107@gmail.com>
2025-09-28 22:02:19 -0300

    nodejs-express-update

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -13,3 +13,15 @@ test('notes are returned as json', async () => {
 afterAll(() => {
     mongoose.connection.close()
 })
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body[0].content).toBe('HTML is easy')
+})
