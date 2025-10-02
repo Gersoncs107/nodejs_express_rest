79712b5b662222cd72d52717e449ab9d5ba5ee39
Gersoncs107 <gerson.silva107@gmail.com>
2025-10-02 19:15:08 -0300

    nodejs-express-update

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helpers = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Note = require('../models/note')
