ddf51c9c83957ac47d190382d412ecd0ba1375ae
Gersoncs107 <gerson.silva107@gmail.com>
2025-10-25 18:43:14 -0300

    nodejs-express-update

--- controllers/login.js
+++ controllers/login.js
@@ -22,7 +22,7 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '1h' })
+  const token = jwt.sign(userForToken, process.env.SECRET)
 
   response
     .status(200)
